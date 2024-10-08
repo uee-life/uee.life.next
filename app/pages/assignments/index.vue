5bd3099de7f4256bc2e45f2506da625e8f72771b
Capn Flint <capnflinttv@gmail.com>
2024-10-07 22:15:17 -0700

    Added a notice if no assignments are found

--- app/pages/assignments/index.vue
+++ app/pages/assignments/index.vue
@@ -7,7 +7,8 @@ const { data, status, refresh } = await useAPI(`/api/assignments`)
 <template>
     <widgets-loading v-if="status == 'pending'"></widgets-loading>
     <div v-if="status == 'success'">
-        <assignment-summary v-for="ass in data.data" :assignment="ass">
+        <assignment-summary v-if="data.data.length > 0" v-for="ass in data.data" :assignment="ass">
         </assignment-summary>
+        <widgets-no-result v-else text="No Assignments Found" />
     </div>
 </template>
