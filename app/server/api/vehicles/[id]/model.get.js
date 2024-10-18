4ca191925afd5507a1b2d14010c8d15b3ae4504e
Capn Flint <capnflinttv@gmail.com>
2024-10-19 00:22:56 +0100

    Fixes

---
+++ app/server/api/vehicles/[id]/model.get.js
@@ -0,0 +1,14 @@
+// Public
+export default defineEventHandler(async (event) => {
+    const id = getRouterParam(event, 'id')
+    const vehicle = await getVehicleModel(id)
+
+    if (vehicle) {
+        // load assignments
+        //vehicle.assignments = await getAllAssignments(vehicle.id, vehicle.owner.id)
+        return apiSuccess(vehicle)
+    } else {
+        return apiError(event, 'Vehicle not found')
+    }
+    
+})
