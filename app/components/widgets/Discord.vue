d0b7a23c62ec9c75273cbd1199c41e050875e9b6
Capn Flint <capnflinttv@gmail.com>
2024-10-23 11:34:53 -0700

    Removed atmo event, added discord QR

---
+++ app/components/widgets/Discord.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+    <div class="event">
+        <a target="_blank" href="https://discord.gg/GjgPGkRDrd">
+            <img class="event-img" src="@/assets/discord.png" />
+        </a>
+    </div>
+</template>
+
+<style scoped>
+.event {
+    margin-bottom: -5px;
+}
+.event-img {
+    width: 100%;
+}
+</style>
